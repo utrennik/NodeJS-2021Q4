390299bb21c6229e8abbb73d47337b4f6fbb911b
Utrennik <utrennik@list.ru>
2021-11-14 13:30:31 +0300

    refactor: refactor config validation, add config format validation

--- src/modules/get-enc-streams.js
+++ src/modules/get-enc-streams.js
@@ -2,37 +2,29 @@ const { Caesar } = require('../streams/caesar');
 const { Atbash } = require('../streams/atbash');
 const { Rot8 } = require('../streams/rot-8');
 const { showError } = require('./show-error');
-
-function isEncodingParamValid(param) {
-  return param === '1' || param === '0';
-}
+const { validateConfig } = require('./validate-config');
 
 function getEncStreams(config) {
+  if (!validateConfig(config)) showError('Error! Config is invalid!');
+
   const argsArr = config.split(/-/);
 
   const streams = argsArr.map((el) => {
     const cypherLetter = el[0];
     const encodingParam = el[1];
     const isEncoding = !!+encodingParam;
-    const isValid = isEncodingParamValid(encodingParam);
 
     switch (cypherLetter) {
       case 'A':
-        if (encodingParam) showError('Error! Config is invalid!');
         return new Atbash();
 
       case 'C': {
-        if (!isValid) showError('Error! Config is invalid!');
         return new Caesar(isEncoding);
       }
 
       case 'R': {
-        if (!isValid) showError('Error! Config is invalid!');
         return new Rot8(isEncoding);
       }
-
-      default:
-        showError('Error! Config is invalid!');
     }
   });
 
