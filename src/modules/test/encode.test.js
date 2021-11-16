1af09377e40e5a6bbbc8616a2255957c54d5297c
Utrennik <utrennik@list.ru>
2021-11-16 19:51:49 +0300

    feat: add ecoding function tests

--- src/modules/test/encode.test.js
+++
@@ -1,8 +0,0 @@
-const { encode } = require('../encode');
-
-describe('Testing encode function', () => {
-  it('Atbash encoding', (done) => {
-    expect(encode('Alex', null, true)).toStrictEqual('Zovc');
-    done();
-  });
-});
